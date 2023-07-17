08f47a4b194416382f6076252c24094cd6a7f1b7
araguaci <araguaci@gmail.com>
2023-07-17 18:13:14 -0300

    Inquérito do Fim do Mundo

--- src/components/PdfViewer.vue
+++ src/components/PdfViewer.vue
@@ -10,7 +10,7 @@ export default {
   methods: {
     openPdf() {
       const file = this.$route.params.file
-      const pdfUrl = '/inquerito_do_fim_do_mundo_o_apagar_das_luzes_do_direito_brasileiro' + file + '.pdf'
+      const pdfUrl = '/inquerito_do_fim_do_mundo_o_apagar_das_luzes_do_direito_brasileiro.pdf'
       // window.open(pdfUrl, '_blank')
     }
   }
